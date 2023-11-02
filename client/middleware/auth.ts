ab91d8fec8921193363d4181d0d0d1dac416732a
Ebubekir Güngör <115069398+ebubekirgungor@users.noreply.github.com>
2023-11-02 23:23:31 +0300

    Update auth.ts

--- client/middleware/auth.ts
+++ client/middleware/auth.ts
@@ -2,6 +2,6 @@ import { useUser } from "~/store/user";
 export default defineNuxtRouteMiddleware((to, from) => {
   const { user } = useUser();
   if (!user.token) {
-    return navigateTo("/?login");
+    return navigateTo("/account");
   }
 });
